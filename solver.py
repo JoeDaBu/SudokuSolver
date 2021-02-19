f12c3520bddcbf5e2a935ce7c08c482443570533
joe <joegbu@gmail.com>
2021-02-19 00:00:10 -0700

    Initial commit

---
+++ solver.py
@@ -0,0 +1,32 @@
+def print_puzzle(puzzle):
+    if type(puzzle) == puzzle:
+        for i in range(9):
+            x = 0
+            while x<9:
+                if x == 0:
+                    print("| ", end="")
+                else:
+                    print(end=" ")
+                print(puzzle[i][x], end="")
+                if (x+1) % 3 == 0 and x != 0:
+                    print(end=" |")
+                if x % 8 == 0 and x != 0:
+                    print()
+                x += 1
+    else:
+        raise Exception("Invalid argument, must be Sudoku puzzel")
+
+x = [[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9]]
+print_puzzle(x)
+
+class Solver:
+    def __init__(self,puzzle):
+        self.solve_puzzle(puzzle)
+    def __repr__(self):
+        return print_puzzle(self.puzzle)
+    def solve_puzzle(self, puzzle):
+        self.puzzle = puzzle
+
+
+
+
